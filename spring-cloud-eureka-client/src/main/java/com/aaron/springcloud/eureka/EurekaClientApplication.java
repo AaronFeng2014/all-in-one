afc85ffabcacf41085bcde27d077e17b2572f01a
albert.feng <albert.feng@pkfare.com>
2018-02-11 11:33:15 +0800

    结构修改

--- spring-cloud-eureka-client/src/main/java/com/aaron/springcloud/eureka/EurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/aaron/springcloud/eureka/EurekaClientApplication.java
@@ -1,8 +1,7 @@
-package eureka;
+package com.aaron.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
