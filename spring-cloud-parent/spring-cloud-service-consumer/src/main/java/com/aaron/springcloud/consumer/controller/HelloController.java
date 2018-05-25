9be0cbcb49803d528ec0f33bb9f13884a8b9d037
冯海鑫 <haixin.feng@ximalaya.com>
2018-05-25 13:54:58 +0800

    logback日志配置文件
    Signed-off-by: 冯海鑫 <haixin.feng@ximalaya.com>

--- spring-cloud-parent/spring-cloud-service-consumer/src/main/java/com/aaron/springcloud/consumer/controller/HelloController.java
+++ spring-cloud-parent/spring-cloud-service-consumer/src/main/java/com/aaron/springcloud/consumer/controller/HelloController.java
@@ -2,9 +2,12 @@ package com.aaron.springcloud.consumer.controller;
 
 import com.aaron.springcloud.api.Student;
 import com.aaron.springcloud.api.UserServiceFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,13 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController
 {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private UserServiceFacade userService;
 
 
-    @RequestMapping ("/hello/{name}")
+    /**
+     * produces = "application/json" 会处理成json
+     *
+     * @param name
+     * @return
+     */
+    @RequestMapping (value = "/hello/{name}", produces = "application/json")
+    @ResponseBody
     public Student sayHello(@PathVariable ("name") String name)
     {
+        LOGGER.error("错误日志测试 {}", name);
+        LOGGER.warn("警告日志测试 {}", name);
+        LOGGER.debug("调试日志测试 {}", name);
+        LOGGER.info("INFO日志测试 {}", name);
+        LOGGER.trace("追踪日志测试 {}", name);
+
         return userService.queryUserInfo(8888L);
     }
 }
